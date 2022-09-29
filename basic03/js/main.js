0630bb9727ab47256a580ed64234459050be8e12
Pilzen <113435159+Pilzen@users.noreply.github.com>
2022-09-29 21:55:10 +0900

    Add files via upload

--- basic03/js/main.js
+++ basic03/js/main.js
@@ -1,12 +1,26 @@
-$('.class_slide').slick({
-    arrows: false,
-    dot: true,
-    autoplay: true
-})
-
 
 $('.top_banner i').on('click', function () {
     $('.top_banner').slideUp();
+});
+
+$('.main_slide').on('init afterChange', function (e, s, c) {
+    console.log(e, s, c);
+    $('.num').text((c ? c + 1 : 1) + "/" + s.slideCount);
+    $('.main_visual .btn li').eq(c).addClass('on').ciblings().remove('on')
 })
 
+// $('.main_visual .btn li').on('click', funtion(){
+//     var idx = $(this).index();
+//     $('.main_slide').slick('slickGoTo', idx)
+// })
+
+$('.main_slide').slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: false,
+    // afterChange: function () {
+    //     console.log('바꼍네...')
+    // }
+});
 
