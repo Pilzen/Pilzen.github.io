5e1e65462b6a9b6d4c03eae7dcbcda039c885270
Pilzen <113435159+Pilzen@users.noreply.github.com>
2022-10-04 21:53:59 +0900

    Add files via upload

--- basic03/js/main.js
+++ basic03/js/main.js
@@ -6,21 +6,29 @@ $('.top_banner i').on('click', function () {
 $('.main_slide').on('init afterChange', function (e, s, c) {
     console.log(e, s, c);
     $('.num').text((c ? c + 1 : 1) + "/" + s.slideCount);
-    $('.main_visual .btn li').eq(c).addClass('on').ciblings().remove('on')
-})
+    $('.main_visual .btn li').eq(c).addClass('on').siblings().removeClass('on');
+    var current = $('.main_slide .slick-current');
+    current.addClass('on').siblings().removeClass('on');
+});
 
-// $('.main_visual .btn li').on('click', funtion(){
-//     var idx = $(this).index();
-//     $('.main_slide').slick('slickGoTo', idx)
-// })
+$('.main_visual .btn li').on('click', function () {
+    var idx = $(this).index();
+    $('.main_slide').slick('slickGoTo', idx);
+});
 
 $('.main_slide').slick({
     dots: true,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 3000,
     pauseOnHover: false,
     // afterChange: function () {
     //     console.log('바꼍네...')
     // }
 });
 
+$('.main_movie i:first-child').on('click', function () {
+    $('.main_movie video').trigger('pause')
+});
+$('.main_movie i:last-child').on('click', function () {
+    $('.main_movie video').trigger('play')
+});
