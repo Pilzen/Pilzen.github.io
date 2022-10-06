40745621e2017885f66912aa57ca3424fe7c23c1
Pilzen <113435159+Pilzen@users.noreply.github.com>
2022-10-06 21:54:05 +0900

    Add files via upload

--- basic03/js/main.js
+++ basic03/js/main.js
@@ -21,9 +21,9 @@ $('.main_slide').slick({
     autoplay: true,
     autoplaySpeed: 3000,
     pauseOnHover: false,
-    // afterChange: function () {
-    //     console.log('바꼍네...')
-    // }
+    afterChange: function () {
+        console.log('바꼍네...')
+    }
 });
 
 $('.main_movie i:first-child').on('click', function () {
@@ -32,3 +32,46 @@ $('.main_movie i:first-child').on('click', function () {
 $('.main_movie i:last-child').on('click', function () {
     $('.main_movie video').trigger('play')
 });
+
+$('#bgmovie').YTPlayer({
+    videoURL: 'https://www.youtube.com/watch?v=Cxzzg7L3Xgc',
+    containment: '.main_ytp',
+    showControls: false,
+    playOnlyIfVisible: true,
+    autoPlay: true,
+    mute: true,
+    startAt: 0, opacity: 1
+});
+
+$('.main_ytp i:first-child').on('click', function () {
+    $('#bgmovie').YTPPause();
+})
+$('.main_ytp i:last-child').on('click', function () {
+    $('#bgmovie').YTPPlay();
+})
+
+/* Product Slide */
+
+$('.product_slide').slick({
+    slidesToShow: 5,
+    centerMode: true, /* padding 효과 */
+    arrows: false,  /* 버튼 삭제 */
+})
+
+$('.product_slide').on('afterChange', function (e, s, c) {
+    $('.main_product .dots li').eq(c).addClass('on').siblings().removeClass('on')
+});
+
+$('.main_product i:first-child').on('click', function () {
+    $('.product_slide').slick('slickPrev')
+});
+
+$('.main_product i:last-child').on('click', function () {
+    $('.product_slide').slick('slickNext')
+});
+
+/* idx 는 li의 Number 0 ~ 5 */
+$('.main_product .dots li').on('click', function () {
+    var idx = $(this).index();
+    $('.product_slide').slick('slickGoTo', idx)
+})
