7974edb4b2f8dc825fea1af4cd8ccf469db2d730
Pilzen <113435159+Pilzen@users.noreply.github.com>
2022-10-20 21:51:41 +0900

    Add files via upload

--- 현대엘리베이트/js/main.js
+++ 현대엘리베이트/js/main.js
@@ -3,4 +3,23 @@ $(function () {
         arrows: false,
         dots: true,
     })
+    $('.top_banner i').on('click', function () {
+        $('.top_banner').slideUp(500)
+    })
 })
+
+$(window).on('scroll', function () {
+    console.log($(window).scrollTop())
+    var sct = $(window).scrollTop()
+    if (sct > 0) {
+        $('.header .bottom').addClass('on')
+    } else {
+        $('.header .bottom').removeClass('on')
+    }
+});
+
+$('.header .global').on('click', function () {
+    $('.header .link').toggleClass('on')
+})
+
+
